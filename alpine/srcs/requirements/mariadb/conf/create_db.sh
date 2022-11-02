0976b5b93e05843641e940890f5bc7969c8be3bc
bmaegan <bmaegan@student.21-school.ru>
2022-11-02 13:17:24 -0400

    develop

--- alpine/srcs/requirements/mariadb/conf/create_db.sh
+++ alpine/srcs/requirements/mariadb/conf/create_db.sh
@@ -11,7 +11,6 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
         mysql_install_db --user=mysql \
 		--basedir=/usr \
 		--datadir=/var/lib/mysql \
-	#	--rpm
 
         tfile=`mktemp`
         if [ ! -f "$tfile" ]; then
@@ -22,17 +21,15 @@ fi
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
 
         cat << EOF > /tmp/create_db.sql
-USE mysql;
 FLUSH PRIVILEGES;
-DELETE FROM     mysql.user WHERE User='';
-DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PWD}';
-CREATE DATABASE wordpress;
-CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PWD}';
-GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
+CREATE DATABASE ${WP_DB_NAME};
+CREATE USER '${DB_ADMIN}'@'localhost' IDENTIFIED BY '${DB_ROOT_PWD}';
+GRANT ALL PRIVILEGES ON *.* TO '${DB_ADMIN}'@'%' WITH GRANT OPTION;
+CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PWD}';
+GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
+-- ALTER USER 'root'@'localhost' IDENTIFIED BY '{DB_ROOT_PWD}';
+-- FLUSH PRIVILEGES;
 EOF
         # run init.sql
         /usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
