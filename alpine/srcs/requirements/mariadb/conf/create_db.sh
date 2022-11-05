bbd529e9dbad865576816fbd346dd312f08640d8
Belia Maegan <bmaegan@il-j1.msk.21-school.ru>
2022-11-05 23:47:01 +0300

    fix

--- alpine/srcs/requirements/mariadb/conf/create_db.sh
+++ alpine/srcs/requirements/mariadb/conf/create_db.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 # execute any pre-init scripts
-for i in /scripts/pre-init.d/*sh
-do
-	if [ -e "${i}" ]; then
-		echo "[i] pre-init.d - processing $i"
-		. "${i}"
-	fi
-done
+# for i in /scripts/pre-init.d/*sh
+# do
+# 	if [ -e "${i}" ]; then
+# 		echo "[i] pre-init.d - processing $i"
+# 		. "${i}"
+# 	fi
+# done
+
 
 if [ -d "/run/mysqld" ]; then
 	echo "[i] mysqld already present, skipping creation"
@@ -23,19 +24,17 @@ if [ -d /var/lib/mysql/mysql ]; then
 	chown -R mysql:mysql /var/lib/mysql
 else
 	echo "[i] MySQL data directory not found, creating initial DBs"
-
 	chown -R mysql:mysql /var/lib/mysql
-
 	mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
 
-	if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
-		MYSQL_ROOT_PASSWORD=`12`
-		echo "[i] MySQL root Password: $MYSQL_ROOT_PASSWORD"
-	fi
+	# if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
+	# 	MYSQL_ROOT_PASSWORD=`12`
+	# 	echo "[i] MySQL root Password: $MYSQL_ROOT_PASSWORD"
+	# fi
 
-	MYSQL_DATABASE=wordpress
-	MYSQL_USER=bmaegan
-	MYSQL_PASSWORD=12
+	# MYSQL_DATABASE=wordpress
+	# MYSQL_USER=bmaegan
+	# MYSQL_PASSWORD=12
 
 	tfile=`mktemp`
 	if [ ! -f "$tfile" ]; then
@@ -45,28 +44,18 @@ else
 	cat << EOF > $tfile
 USE mysql;
 FLUSH PRIVILEGES ;
-GRANT ALL ON *.* TO 'root'@'%' identified by '12' WITH GRANT OPTION ;
-GRANT ALL ON *.* TO 'root'@'localhost' identified by '12' WITH GRANT OPTION ;
-SET PASSWORD FOR 'root'@'localhost'=PASSWORD('12') ;
-DROP DATABASE IF EXISTS test ;
+GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
+GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
+SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
 FLUSH PRIVILEGES ;
+DROP DATABASE IF EXISTS test ;
+
+CREATE DATABASE IF NOT EXISTS '$MYSQL_DATABASE' CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL ON ${MYSQL_DATABASE}.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+FLUSH PRIVILEGES ;
+
 EOF
-
-	if [ "$MYSQL_DATABASE" != "" ]; then
-	    echo "[i] Creating database: wordpress"
-		if [ "$MYSQL_CHARSET" != "" ] && [ "$MYSQL_COLLATION" != "" ]; then
-			echo "[i] with character set [utf8]]"
-			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET $MYSQL_CHARSET COLLATE $MYSQL_COLLATION;" >> $tfile
-		else
-			echo "[i] with character set: 'utf8' and collation: 'utf8_general_ci'"
-			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
-		fi
-
-	 if [ "$MYSQL_USER" != "" ]; then
-		echo "[i] Creating user: bmaegan with password 12"
-		echo "GRANT ALL ON \`wordpress\`.* to 'bmaegan'@'%' IDENTIFIED BY '$12';" >> $tfile
-	    fi
-	fi
 
 	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < $tfile
 	rm -f $tfile
