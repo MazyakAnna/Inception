58989487dfd418f92f54aa2e79d0f9607dd1cd82
Belia Maegan <bmaegan@il-j1.msk.21-school.ru>
2022-11-06 01:33:44 +0300

    fix

--- alpine/srcs/requirements/mariadb/conf/create_db.sh
+++ alpine/srcs/requirements/mariadb/conf/create_db.sh
@@ -1,22 +1,19 @@
 #!/bin/sh
 
+echo "Check mysqld dir"
 if [ -d "/run/mysqld" ]; then
-	echo "[i] mysqld already present, skipping creation"
 	chown -R mysql:mysql /run/mysqld
 else
-	echo "[i] mysqld not found, creating...."
 	mkdir -p /run/mysqld
 	chown -R mysql:mysql /run/mysqld
 fi
 
+echo "Check dir with initial DBs"
 if [ -d /var/lib/mysql/mysql ]; then
-	echo "[i] MySQL directory already present, skipping creation"
 	chown -R mysql:mysql /var/lib/mysql
 else
-	echo "[i] MySQL data directory not found, creating initial DBs"
 	chown -R mysql:mysql /var/lib/mysql
 	mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
-
 
 	sql_script=`script.sql`
 	if [ ! -f "$sql_script" ]; then
@@ -26,6 +23,7 @@ else
 	cat << EOF > $sql_script
 USE mysql ;
 FLUSH PRIVILEGES ;
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci ;
 
 GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
 GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
@@ -33,14 +31,12 @@ SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
 SET PASSWORD FOR 'root'@'%'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
 FLUSH PRIVILEGES ;
 
-CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci ;
 CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
 CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
 FLUSH PRIVILEGES ;
 EOF
 	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < $sql_script
 	rm -f $sql_script
 fi
-
