b7ff1b9f1cd2b390469d3442d9e9c2f99439b87c
Belia Maegan <bmaegan@il-j1.msk.21-school.ru>
2022-11-06 15:27:14 +0300

    +1

--- alpine/srcs/requirements/wordpress/tools/create_wpsite.sh
+++ alpine/srcs/requirements/wordpress/tools/create_wpsite.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 # while ! mariadb -h $WORDPRESS_DB_HOST -P 3306 -u $WORDPRESS_DB_USER -p $WORDPRESS_DB_PASSWORD; do echo "waiting for db ..."; done
+# wp config create
+
+
 wp core install --url="$DOMAIN_NAME" --title="random title" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL" --skip-email --allow-root
 wp plugin install hello-dolly --activate
 wp theme install twentytwenty --activate
