0befd478d4a5cb94a3ffb24a952e501d7306a3bb
Belia Maegan <bmaegan@il-j1.msk.21-school.ru>
2022-11-06 05:48:35 +0300

    new

--- useful_scripts/docker_cleaner.sh
+++ useful_scripts/docker_cleaner.sh
@@ -4,7 +4,4 @@ docker rm $(docker ps -qa)
 docker rmi -f $(docker images -qa)
 docker volume rm $(docker volume ls -q)
 docker network rm $(docker network ls -q) 2>/dev/null
-sudo rm -rf /home/bmaegan/data/mariadb/* 
-sudo rm -rf /home/bmaegan/data/MariaDB/*
-sudo rm -rf /home/bmaegan/data/wordpress/*
-sudo rm -rf /home/bmaegan/data/mariaDB/*
+sudo rm -rf /home/bmaegan/data/mariadb/* /home/bmaegan/data/MariaDB/* /home/bmaegan/data/wordpress/* /home/bmaegan/data/mariaDB/*
