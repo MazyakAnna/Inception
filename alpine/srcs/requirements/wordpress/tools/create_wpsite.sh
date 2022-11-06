f026375d048ff31b70200ceb5b6c4a6fc0fc0745
Belia Maegan <bmaegan@il-j1.msk.21-school.ru>
2022-11-06 15:19:28 +0300

    +1

--- alpine/srcs/requirements/wordpress/tools/create_wpsite.sh
+++ alpine/srcs/requirements/wordpress/tools/create_wpsite.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-while ! mariadb -h $MYSQL_HOST -P 3306 -u $MYSQL_USER -p $MYSQL_PASSWORD; do echo "waiting for db ..."; done
-wp core install --url="$DOMAIN" --title="random title" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL" --skip-email
+# while ! mariadb -h $WORDPRESS_DB_HOST -P 3306 -u $WORDPRESS_DB_USER -p $WORDPRESS_DB_PASSWORD; do echo "waiting for db ..."; done
+wp core install --url="$DOMAIN_NAME" --title="random title" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL" --skip-email --allow-root
 wp plugin install hello-dolly --activate
 wp theme install twentytwenty --activate
 wp plugin update --all
-wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASSWORD
+wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASSWORD  --allow-root;
 wp post create --post_title="RANDOM TITLE" --post_content="RANDOM CONTENT" --post_status=publish --post_author="$WORDPRESS_USER"
+wp theme install inspiro --activate --allow-root
