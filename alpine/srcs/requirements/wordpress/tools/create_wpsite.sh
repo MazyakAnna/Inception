126cd27b0509b94530b31b54b145285164bc6f8d
Belia Maegan <bmaegan@il-j1.msk.21-school.ru>
2022-11-06 23:04:10 +0300

    +1

--- alpine/srcs/requirements/wordpress/tools/create_wpsite.sh
+++ alpine/srcs/requirements/wordpress/tools/create_wpsite.sh
@@ -27,7 +27,7 @@ define( 'WP_REDIS_READ_TIMEOUT', 1 );
 define( 'WP_REDIS_DATABASE', 0 );
 PHP
 
-    cat /tmp/conf/exported_database.sql | wp db import
+    wp db import /tmp/conf/exported_database.sql --path='./wordpress'
     wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASSWORD  --allow-root;
     wp user create $WORDPRESS_ADMIN_USER $WORDPRESS_ADMIN_EMAIL --role=administrator --user_pass=$WORDPRESS_ADMIN_PASSWORD  --allow-root;
 
