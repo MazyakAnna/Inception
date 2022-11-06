701d7ddae0ed0a05b1ccee6624dc57ab3761f9ed
Belia Maegan <bmaegan@il-j1.msk.21-school.ru>
2022-11-06 21:39:37 +0300

    +1

--- alpine/srcs/requirements/wordpress/tools/create_wpsite.sh
+++ alpine/srcs/requirements/wordpress/tools/create_wpsite.sh
@@ -14,48 +14,29 @@ if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
     echo "Wordpress: creating users..."
 # Создание таблицы WordPress в базе данных, 
 # используя URL-адрес, заголовок и предоставленные данные пользователя-администратора по умолчанию
-    # wp core install --url="$DOMAIN_NAME" --title="$WORDPRESS_DB_NAME" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL" --path="/var/www/html/wordpress/" --skip-email --allow-root
-    wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_ADMIN_USER --dbpass=$WORDPRESS_ADMIN_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbprefix=$WORDPRESS_TABLE_PREFIX --path="/var/www/html/wordpress"  --config-file="wp-config.php" --allow-root;
-#     --extra-php << PHP
-# define( 'WP_DEBUG', true );
-# define( 'WP_DEBUG_LOG', true );
-# define( 'WP_SITEURL', 'https://$DOMAIN_NAME' );
-# define('WP_HOME', 'https://$DOMAIN_NAME'); 
-# define( 'WP_REDIS_HOST', '$REDIS_HOST' );
-# define( 'WP_REDIS_PASSWORD', '$REDIS_PASSWORD' );
-# define( 'WP_REDIS_PORT', 6379 );
-# define( 'WP_REDIS_TIMEOUT', 1 );
-# define( 'WP_REDIS_READ_TIMEOUT', 1 );
-# define( 'WP_REDIS_DATABASE', 0 );
-# PHP;
+    # wp core install --url="$DOMAIN_NAME" --title="$WORDPRESS_DB_NAME" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL" --path="wordpress/" --skip-email --allow-root
+    wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_ADMIN_USER --dbpass=$WORDPRESS_ADMIN_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbprefix=$WORDPRESS_TABLE_PREFIX --path="/var/www/html/wordpress"  --config-file="wp-config.php" --allow-root --extra-php << PHP
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
+define( 'WP_SITEURL', 'https://$DOMAIN_NAME' );
+define('WP_HOME', 'https://$DOMAIN_NAME'); 
+define( 'WP_REDIS_HOST', '$REDIS_HOST' );
+define( 'WP_REDIS_PORT', 6379 );
+define( 'WP_REDIS_TIMEOUT', 1 );
+define( 'WP_REDIS_READ_TIMEOUT', 1 );
+define( 'WP_REDIS_DATABASE', 0 );
+PHP
+
 # --admin_email="$WORDPRESS_ADMIN_EMAIL"
     wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASSWORD  --allow-root;
 # Тема для WordPress
 wp theme install inspiro --activate --allow-root
 
 
-# wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbprefix=$WORDPRESS_TABLE_PREFIX --config-file="/var/www/html/wordpress/wp-config.php"
-#  --extra-php <<PHP
-# define( 'WP_DEBUG', true );
-# define( 'WP_DEBUG_LOG', true );
-# define( 'WP_REDIS_HOST', '${REDIS_HOST}' );
-# define( 'WP_REDIS_PASSWORD', '${REDIS_PASSWORD}' );
-# define( 'WP_REDIS_PORT', 6379 );
-# define( 'WP_REDIS_TIMEOUT', 1 );
-# define( 'WP_REDIS_READ_TIMEOUT', 1 );
-# define( 'WP_REDIS_DATABASE', 0 );
-# PHP
+# enable redis cache
 
-# # enable redis cache
-#     sed -i "40i define( 'WP_REDIS_HOST', '$REDIS_HOST' );"      wp-config.php
-#     sed -i "41i define( 'WP_REDIS_PORT', 6379 );"               wp-config.php
-#     #sed -i "42i define( 'WP_REDIS_PASSWORD', '$REDIS_PWD' );"   wp-config.php
-#     sed -i "42i define( 'WP_REDIS_TIMEOUT', 1 );"               wp-config.php
-#     sed -i "43i define( 'WP_REDIS_READ_TIMEOUT', 1 );"          wp-config.php
-#     sed -i "44i define( 'WP_REDIS_DATABASE', 0 );\n"            wp-config.php
-
-    # wp plugin install redis-cache --activate --allow-root
-    # wp plugin update --all --allow-root
+    wp plugin install redis-cache --activate --allow-root
+    wp plugin update --all --allow-root
 
 echo "Wordpress: set up!"
 else
