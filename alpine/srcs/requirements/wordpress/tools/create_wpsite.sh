bf6b2260e0a71018ddea739aa378a9114b743140
Belia Maegan <bmaegan@il-j1.msk.21-school.ru>
2022-11-06 15:37:16 +0300

    +1

--- alpine/srcs/requirements/wordpress/tools/create_wpsite.sh
+++ alpine/srcs/requirements/wordpress/tools/create_wpsite.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
 # while ! mariadb -h $WORDPRESS_DB_HOST -P 3306 -u $WORDPRESS_DB_USER -p $WORDPRESS_DB_PASSWORD; do echo "waiting for db ..."; done
-# wp config create
-
+# [--config-file=<path>] 
+wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbprefix=$WORDPRESS_TABLE_PREFIX --extra-php <<PHP
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
+define( 'WP_REDIS_HOST', '${REDIS_HOST}' );
+define( 'WP_REDIS_PASSWORD', '${REDIS_PASSWORD}' );
+define( 'WP_REDIS_PORT', 6379 );
+define( 'WP_REDIS_TIMEOUT', 1 );
+define( 'WP_REDIS_READ_TIMEOUT', 1 );
+define( 'WP_REDIS_DATABASE', 0 );
+PHP
 
 wp core install --url="$DOMAIN_NAME" --title="random title" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL" --skip-email --allow-root
 wp plugin install hello-dolly --activate
