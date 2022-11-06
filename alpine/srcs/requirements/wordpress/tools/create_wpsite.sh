bf9dbbc9605163c5b1e1271fdf8dc0ea6af580a7
Belia Maegan <bmaegan@il-j1.msk.21-school.ru>
2022-11-06 15:05:00 +0300

    +1

--- alpine/srcs/requirements/wordpress/tools/create_wpsite.sh
+++ alpine/srcs/requirements/wordpress/tools/create_wpsite.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-while ! mariadb -h$MYSQL_HOST -P${MYSQL_PORT} -u$MYSQL_USER -p$MYSQL_PASSWORD; do echo "waiting for db ..."; done
+while ! mariadb -h $MYSQL_HOST -P 3306 -u $MYSQL_USER -p $MYSQL_PASSWORD; do echo "waiting for db ..."; done
 wp core install --url="$DOMAIN" --title="random title" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL" --skip-email
 wp plugin install hello-dolly --activate
 wp theme install twentytwenty --activate
