78d5327a7401733c40fe7b4cca4d729dad959ef3
bmaegan <bmaegan@student.21-school.ru>
2022-11-11 01:10:40 -0500

    kek

--- debian/srcs/requirements/mariadb/conf/create_db.sh
+++ debian/srcs/requirements/mariadb/conf/create_db.sh
@@ -1,24 +1,8 @@
 #!/bin/sh
 
-echo "Check mysqld dir"
-if [ -d "/run/mysqld" ]; then
-	chown -R mysql:mysql /run/mysqld
-else
-	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld
-fi
-
-echo "Check dir with initial DBs"
-if [ -d /var/lib/mysql/mysql ]; then
-	chown -R mysql:mysql /var/lib/mysql
-	echo "WE DO ALREADY HAVE SMTH"
-else
-	chown -R mysql:mysql /var/lib/mysql
-	mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
-fi
-
 if ! [ -d /var/lib/mysql/wordpress]; then
 	echo "LET'S CREATE THE WP DB"
 	sed -i 's/bind-address            = 127.0.0.1/ bind-address            = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
-	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0  < /tmp/create_db.sql
+	service mysqld start
+	/usr/bin/mysql  < /tmp/create_db.sql;
 fi
--- debian/srcs/requirements/wordpress/tools/create_wpsite.sh
+++ debian/srcs/requirements/wordpress/tools/create_wpsite.sh
@@ -12,10 +12,8 @@ if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
     # wp core download --allow-root;
     # mv /var/www/wp-config.php /var/www/html/wordpress;
     echo "Wordpress: creating users..."
-# Создание таблицы WordPress в базе данных, 
-# используя URL-адрес, заголовок и предоставленные данные пользователя-администратора по умолчанию
-    # wp core install --url="$DOMAIN_NAME" --title="$WORDPRESS_DB_NAME" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL" --path="wordpress/" --skip-email --allow-root
-    wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_ADMIN_USER --dbpass=$WORDPRESS_ADMIN_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbprefix=$WORDPRESS_TABLE_PREFIX --path="/var/www/html/wordpress"  --config-file="wp-config.php" --allow-root --extra-php << PHP
+## wp core install --url="$DOMAIN_NAME" --title="$WORDPRESS_DB_NAME" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL" --path="wordpress/" --skip-email --allow-root
+    wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=root --dbpass="$MYSQL_ROOT_PASSWORD" --dbhost=$WORDPRESS_DB_HOST --dbprefix=$WORDPRESS_TABLE_PREFIX --path="/var/www/html/wordpress"  --config-file="wp-config.php" --allow-root --extra-php << PHP
 define( 'WP_SITEURL', 'https://$DOMAIN_NAME' );
 define('WP_HOME', 'https://$DOMAIN_NAME'); 
 define( 'WP_REDIS_HOST', 'redis' );
@@ -25,7 +23,7 @@ define( 'WP_REDIS_READ_TIMEOUT', 1 );
 define( 'WP_REDIS_DATABASE', 0 );
 PHP
 
-    wp db import /tmp/conf/exported_database.sql --path='/var/www/html//wordpress'
+#    wp db import /tmp/conf/exported_database.sql --path='/var/www/html//wordpress'
     wp user create "$WORDPRESS_USER" "$WORDPRESS_USER_EMAIL" --role=author --user_pass="$WORDPRESS_USER_PASSWORD"  --path="/var/www/html/wordpress" --allow-root;
     wp user create "$WORDPRESS_ADMIN_USER" "$WORDPRESS_ADMIN_EMAIL" --role=administrator --user_pass="$WORDPRESS_ADMIN_PASSWORD" --path="/var/www/html/wordpress" --allow-root;
 
