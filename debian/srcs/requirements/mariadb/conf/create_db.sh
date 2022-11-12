8ede0ab8b36cfd8c49d615a733994e3250fdc303
Belia Maegan <bmaegan@oa-c1.msk.21-school.ru>
2022-11-12 05:46:30 +0300

    fix

--- debian/srcs/requirements/mariadb/conf/create_db.sh
+++ debian/srcs/requirements/mariadb/conf/create_db.sh
@@ -3,9 +3,10 @@
 #if ! [ -d /var/lib/mysql/wordpress ]; then
 	echo "LET'S CREATE THE WP DB"
 	sed -i 's/bind-address            = 127.0.0.1/ bind-address            = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
-
+	touch /tmp/create_db.sql && chmod 777 /tmp/create_db.sql
 	cat << EOF > /tmp/create_db.sql
 USE mysql ;
+
 FLUSH PRIVILEGES ;
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE_NAME;
 
