0bcd5fe2f4f7ef34b9deb9c623415b11383cac44
Belia Maegan <bmaegan@at-d3.msk.21-school.ru>
2022-11-12 18:58:32 +0300

    wp config logs

--- debian/srcs/requirements/wordpress/tools/create_wpsite.sh
+++ debian/srcs/requirements/wordpress/tools/create_wpsite.sh
@@ -12,7 +12,7 @@ echo "No config, let's create one!"
     # mv /var/www/index.html /var/www/html/wordpress/staticwebsite/index.html;
 
 #    wp core install --url="$DOMAIN_NAME" --title="$WORDPRESS_DB_NAME" --admin_user="root" --admin_password="$MYSQL_ROOT_PASSWORD" --admin_email="$WORDPRESS_ROOT_EMAIL" --path="/var/www/html/wordpress" --skip-email --allow-root
-    wp config create --dbname=$WORDPRESS_DB_NAME --dbuser="$WORDPRESS_ADMIN_USER" --dbpass="$WORDPRESS_ADMIN_PASSWORD" --dbhost=$WORDPRESS_DB_HOST --dbprefix=$WORDPRESS_TABLE_PREFIX --path="/var/www/html/wordpress"  --config-file="wp-config.php" --allow-root --extra-php << PHP
+    wp config create --dbname="$WORDPRESS_DB_NAME" --dbuser="$WORDPRESS_ADMIN_USER" --dbpass="$WORDPRESS_ADMIN_PASSWORD" --dbhost=$WORDPRESS_DB_HOST --dbprefix=$WORDPRESS_TABLE_PREFIX --path="/var/www/html/wordpress"  --config-file="wp-config.php" --allow-root --extra-php << PHP
 define( 'WP_SITEURL', 'https://$DOMAIN_NAME' );
 define( 'WP_HOME', 'https://$DOMAIN_NAME' ); 
 define( 'WP_REDIS_HOST', 'redis' );
@@ -20,20 +20,30 @@ define( 'WP_REDIS_PORT', 6379 );
 define( 'WP_REDIS_TIMEOUT', 1 );
 define( 'WP_REDIS_READ_TIMEOUT', 1 );
 define( 'WP_REDIS_DATABASE', 0 );
+define('WP_CACHE', true);
 PHP
 
+    echo "Config created!"
+
     wp core install --url="$DOMAIN_NAME" --title="$WORDPRESS_DB_NAME" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL" --path="/var/www/html/wordpress" --skip-email --allow-root
+    echo "Base installation Finished"
     wp db import /tmp/conf/exported_database.sql --path='/var/www/html//wordpress' --allow-root;
+    echo "Configured DB imported"
     wp user create "$WORDPRESS_USER" "$WORDPRESS_USER_EMAIL" --role=author --user_pass="$WORDPRESS_USER_PASSWORD"  --path="/var/www/html/wordpress" --allow-root;
     wp user create "$WORDPRESS_ADMIN_USER" "$WORDPRESS_ADMIN_EMAIL" --role=administrator --user_pass="$WORDPRESS_ADMIN_PASSWORD" --path="/var/www/html/wordpress" --allow-root;
+    echo "My fav users created"
     sed -i 's|<h1 class="alignwide" style="margin-bottom:var(--wp--preset--spacing--60)">Mindblown: a blog about philosophy.</h1>|<h1 class="alignwide" style="margin-bottom:var(--wp--preset--spacing--60)">WOW! Look! This Inception project is working!</h1>|g' /var/www/html/wordpress/wp-content/themes/twentytwentythree/templates/home.html
+    echo "Hello world changed"
 
 # install redis plagin
-    wp plugin install redis-cache --activate --allow-root
+    wp plugin install redis-cache --allow-root
+    echo "redist installed"
     wp plugin update --all --allow-root
-
+    echo "redist updated"
+    wp plugin activate redis-cache --allow-root
+    echo "redist activated"
 fi
-wp redis enable --allow-root
+# wp redis-cache enable --allow-root
 
 echo "Wordpress config: OK"
 php-fpm7.3 -F
