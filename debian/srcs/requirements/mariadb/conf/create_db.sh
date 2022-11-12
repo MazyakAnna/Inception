839d2a1b1007dbd674670d71759dffed403607b7
bmaegan <bmaegan@student.21-school.ru>
2022-11-11 19:05:42 -0500

    debian works just fine

--- debian/srcs/requirements/mariadb/conf/create_db.sh
+++ debian/srcs/requirements/mariadb/conf/create_db.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 
-if ! [ -d /var/lib/mysql/wordpress]; then
+#if ! [ -d /var/lib/mysql/wordpress ]; then
 	echo "LET'S CREATE THE WP DB"
 	sed -i 's/bind-address            = 127.0.0.1/ bind-address            = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
-	service mysqld start
-	/usr/bin/mysql  < /tmp/create_db.sql;
-fi
+
+	cat << EOF > /tmp/create_db.sql
+USE mysql ;
+FLUSH PRIVILEGES ;
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE_NAME;
+
+GRANT ALL ON *.* TO 'root'@'%' identified by '${MYSQL_ROOT_PASSWORD}' WITH GRANT OPTION ;
+GRANT ALL ON *.* TO 'root'@'localhost' identified by '${MYSQL_ROOT_PASSWORD}' WITH GRANT OPTION ;
+SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
+SET PASSWORD FOR 'root'@'%'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
+
+CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
+CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE_NAME.* TO '${MYSQL_USER}'@'localhost';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE_NAME.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES ;
+EOF
+
+#	chmod 777 /tmp/create_db.sql && service mysql start && mysql < /tmp/create_db.sql --verbose
+#	rm -rf /tmp/create_db.sql ;
+#fi
--- debian/srcs/requirements/wordpress/tools/create_wpsite.sh
+++ debian/srcs/requirements/wordpress/tools/create_wpsite.sh
@@ -2,20 +2,19 @@
 # while ! mariadb -h$WORDPRESS_DB_HOST -P3306 -u$WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD; do echo "waiting for db ..."; done
 
 echo "CHECK"
-
+mkdir -p /run/php/
 if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
-    echo "No config, let's create one!"
+    
+echo "No config, let's create one!"
     cd /var/www/html/wordpress;
 # static website
-	# mkdir -p /var/www/html/wordpress/staticwebsite;
+# mkdir -p /var/www/html/wordpress/staticwebsite;
     # mv /var/www/index.html /var/www/html/wordpress/staticwebsite/index.html;
-    # wp core download --allow-root;
-    # mv /var/www/wp-config.php /var/www/html/wordpress;
-    echo "Wordpress: creating users..."
-## wp core install --url="$DOMAIN_NAME" --title="$WORDPRESS_DB_NAME" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL" --path="wordpress/" --skip-email --allow-root
-    wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=root --dbpass="$MYSQL_ROOT_PASSWORD" --dbhost=$WORDPRESS_DB_HOST --dbprefix=$WORDPRESS_TABLE_PREFIX --path="/var/www/html/wordpress"  --config-file="wp-config.php" --allow-root --extra-php << PHP
+
+#    wp core install --url="$DOMAIN_NAME" --title="$WORDPRESS_DB_NAME" --admin_user="root" --admin_password="$MYSQL_ROOT_PASSWORD" --admin_email="$WORDPRESS_ROOT_EMAIL" --path="/var/www/html/wordpress" --skip-email --allow-root
+    wp config create --dbname=$WORDPRESS_DB_NAME --dbuser="$WORDPRESS_ADMIN_USER" --dbpass="$WORDPRESS_ADMIN_PASSWORD" --dbhost=$WORDPRESS_DB_HOST --dbprefix=$WORDPRESS_TABLE_PREFIX --path="/var/www/html/wordpress"  --config-file="wp-config.php" --allow-root --extra-php << PHP
 define( 'WP_SITEURL', 'https://$DOMAIN_NAME' );
-define('WP_HOME', 'https://$DOMAIN_NAME'); 
+define( 'WP_HOME', 'https://$DOMAIN_NAME' ); 
 define( 'WP_REDIS_HOST', 'redis' );
 define( 'WP_REDIS_PORT', 6379 );
 define( 'WP_REDIS_TIMEOUT', 1 );
@@ -23,7 +22,8 @@ define( 'WP_REDIS_READ_TIMEOUT', 1 );
 define( 'WP_REDIS_DATABASE', 0 );
 PHP
 
-#    wp db import /tmp/conf/exported_database.sql --path='/var/www/html//wordpress'
+    wp core install --url="$DOMAIN_NAME" --title="$WORDPRESS_DB_NAME" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL" --path="/var/www/html/wordpress" --skip-email --allow-root
+    wp db import /tmp/conf/exported_database.sql --path='/var/www/html//wordpress' --allow-root;
     wp user create "$WORDPRESS_USER" "$WORDPRESS_USER_EMAIL" --role=author --user_pass="$WORDPRESS_USER_PASSWORD"  --path="/var/www/html/wordpress" --allow-root;
     wp user create "$WORDPRESS_ADMIN_USER" "$WORDPRESS_ADMIN_EMAIL" --role=administrator --user_pass="$WORDPRESS_ADMIN_PASSWORD" --path="/var/www/html/wordpress" --allow-root;
 
@@ -34,4 +34,4 @@ PHP
 fi
 
 echo "Wordpress config: OK"
-/usr/sbin/php-fpm -F
+php-fpm7.3 -F
